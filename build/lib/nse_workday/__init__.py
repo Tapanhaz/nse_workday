ca4cdcd28a971d11f478f7c83dbf0382f0c6559d
Tapanhaz <91151267+Tapanhaz@users.noreply.github.com>
2025-01-01 23:54:42 +0530

    Delete build/lib/nse_workday directory

--- build/lib/nse_workday/__init__.py
+++
@@ -1,3 +0,0 @@
-from .nse_workday import workday, isHoliday, get_holidays_list, get_workdays_list, month_last_weekday, get_month_weekdays, get_weekdays, get_new_holidays, update_holiday
-
-__all__ = ['workday', 'isHoliday', 'get_holidays_list', 'get_workdays_list', 'month_last_weekday', 'get_month_weekdays', 'get_weekdays', 'get_new_holidays', 'update_holiday']
